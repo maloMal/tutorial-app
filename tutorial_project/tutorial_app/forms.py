8cb2e94651c09a73ed4fb9262db844248c8a24d9
mbrunson <mbrunson84@gmail.com>
2016-10-27 21:11:03 -0400

    class 10/27/2016

---
+++ tutorial_project/tutorial_app/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from models import Page, Category
+
+
+class CategoryForm(forms.ModelForm):
+	name = forms.CharField(max_length=128, help_text='Please enter a category name!')
+	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	slug = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+	class Meta:
+		model = Category
+		fields = ('name',)
+class PageForm(forms.ModelForm):
+	title = forms.CharField(max_length=128, help_text='Please enter a page name!')	
+	url = forms.URLField(max_length=200, help_text="please enter page URL!")
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+
+
+		if url and not url.startswith('http://'):
+			url = 'http://'+ url
+			cleaned_data['url'] = url
+		return cleaned_data
+	class Meta:
+		model = Page
+		exclude = ('category',)
--- tutorial_project/tutorial_app/views.py
+++ tutorial_project/tutorial_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
 from models import Category, Page
+from forms import CategoryForm, PageForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')
@@ -28,5 +27,43 @@ def category(request, category_name_slug):
 		pass
 
 	return render(request, 'category.html', context_dict)
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print form.errors
+	else: 
+		form = CategoryForm()
+	return render(request, 'add_category.html', {'form':form})	
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+		if form.is_valid():
+			if cat: 
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request, category_name_slug)
+			else: print form.errors
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+
+	context_dict = {'form': form, 'category': cat, 'slug': category_name_slug}
+	return render(request, 'add_page.html', context_dict)
+
+
+
 # Create your views here.
 
